6e6d4dcac0bc48b50723a0913a96f8dd691ac1b4
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-20 11:39:05 +0100

    message

--- src/components/Muinavbar.js
+++ src/components/Muinavbar.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import { Drawer, Box, Typography, IconButton } from "@mui/material"
 import { AiOutlineMenu, AiOutlineClose} from 'react-icons/ai'
-import { MdHouse } from 'react-icons/md'
-import { MdPermContactCalendar } from 'react-icons/md'
-import { BsTelephone, BsFillQuestionSquareFill } from 'react-icons/bs'
+import { GrHome } from 'react-icons/gr'
+import { MdOutlineContacts } from 'react-icons/md'
+import { BsTelephone } from 'react-icons/bs'
+import { RiQuestionnaireLine } from 'react-icons/ri'
 import { AiOutlineUserAdd, AiOutlineUser } from 'react-icons/ai';
 import { Link } from 'react-router-dom'
 import '../css/Navbar.css';
@@ -21,9 +22,9 @@ const Muinavbar = () => {
             </div>
             <Box p={1} width='100%' textAlign='left'>
                 <Typography variant='h6' component='div'>
-                    <li className='li x'><Link to="/" onClick={()=> setOpenDrawer(false)}><MdHouse  style={{marginRight: '10px'}}/>Home</Link></li>
-                    <li className='li x'><Link to="/about" onClick={()=> setOpenDrawer(false)}><MdPermContactCalendar style={{marginRight: '10px'}}/>About</Link></li>
-                    <li className='li x'><Link to="/faqs" onClick={()=> setOpenDrawer(false)}><BsFillQuestionSquareFill style={{marginRight: '10px'}}/>FAQ</Link></li>
+                    <li className='li x'><Link to="/" onClick={()=> setOpenDrawer(false)}><GrHome style={{marginRight: '10px'}}/>Home</Link></li>
+                    <li className='li x'><Link to="/about" onClick={()=> setOpenDrawer(false)}><MdOutlineContacts style={{marginRight: '10px'}}/>About</Link></li>
+                    <li className='li x'><Link to="/faqs" onClick={()=> setOpenDrawer(false)}><RiQuestionnaireLine style={{marginRight: '10px'}}/>FAQ</Link></li>
                     <li className='li x'><Link to="/contact" onClick={()=> setOpenDrawer(false)}><BsTelephone style={{marginRight: '10px'}}/>Contact</Link></li>
                     <li className='li x'><Link to="/login" onClick={()=> setOpenDrawer(false)}><AiOutlineUser style={{marginRight: '10px'}}/>Login</Link></li>
                     <li className='li x'><Link to="/register" onClick={()=> setOpenDrawer(false)}><AiOutlineUserAdd style={{marginRight: '10px'}}/>Register</Link></li>
