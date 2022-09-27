6ae8eb4dca30a706e8fb04deb6196db1a15697f3
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-27 15:21:35 +0100

    message

--- src/components/Testimonials.js
+++ src/components/Testimonials.js
@@ -3,9 +3,12 @@ import OwlCarousel from 'react-owl-carousel';
 import 'owl.carousel/dist/assets/owl.carousel.css';  
 import 'owl.carousel/dist/assets/owl.theme.default.css';  
 import '../css/Testimonials.css'
-import img from '../assets/cryptobg.jpg'
+// import img from '../assets/cryptobg.jpg'
 import logo from '../assets/logo.png'
 import { AiFillStar } from 'react-icons/ai'
+import img1 from '../assets/test1.png'
+import img3 from '../assets/test3.jpg'
+import img2 from '../assets/test2.jpg'
 
 const Testimonials = () => {
   return (
@@ -29,12 +32,34 @@ const Testimonials = () => {
             // }
             >
                 <div>
-                  <img  className="img" src= {img} style={{width: '200px', height: '200px', margin: 'auto'}} alt='graphics'/>
-                  <h3>John Doe</h3>
+                  <img  className="img" src= {img1} style={{width: '200px', height: '200px', margin: 'auto', border: '3px solid rgb(35, 201, 216)'}} alt='graphics'/>
+                  <h3>Mark Richard</h3>
+                  <strong>M.D at Middew PVT</strong>
+                  <div className='icon-cont'><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/></div>
+                  <p>
+                    I decided to invest some dollars hoping i would get my profit as stated on the website. The intetresting part is 
+                    when I saw my money growing I decided to pick up the platinum weekly plan. This is too real to be true. Then I was
+                    contacted by the admin to write a testimonial about the platform in wich I gladly did. This is real.
+                  </p>
+                </div> 
+                <div>
+                  <img  className="img" src= {img3} style={{width: '200px', height: '200px', margin: 'auto', border: '3px solid rgb(35, 201, 216)'}} alt='graphics'/>
+                  <h3>Catherine K.</h3>
+                  <strong>Product Manager</strong>
+                  <div className='icon-cont'><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/></div>
+                  <p>
+                    These guys are unbelievably amazing. At first, I was skeptical until I tried them out. I recommend them. Thum up to you guys.
+                  </p>
+                </div> 
+                <div>
+                  <img  className="img" src= {img2} style={{width: '200px', height: '200px', margin: 'auto', border: '3px solid rgb(35, 201, 216)'}} alt='graphics'/>
+                  <h3>John Stone</h3>
                   <strong>Business Analyst</strong>
                   <div className='icon-cont'><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/><AiFillStar/></div>
                   <p>
-                    Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptatibus, eos quidem debitis assumenda molestiae tempora fugit totam labore qui consectetur est nihil asperiores ipsum reiciendis inventore aliquid temporibus perferendis quae tempore, laboriosam explicabo libero a! Praesentium, enim modi! Illum commodi dicta nesciunt quis quasi esse recusandae expedita, cumque quisquam itaque!
+                    It is very easy to make investment on this site. They have different payment gateways that are secured and easy to use. I have
+                    also earned from their referral program. I upgraded my membership plan to Gold and it has been fun with lots of features added
+                    to my account. One thing I must say is that you don't need money to start making money from this site. Thanks.
                   </p>
                 </div> 
         </OwlCarousel>
