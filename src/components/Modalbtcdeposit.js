7efec4a967ea8c19402933062974f2d75c5e5d81
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-09-29 23:13:33 +0100

    message

--- src/components/Modalbtcdeposit.js
+++ src/components/Modalbtcdeposit.js
@@ -1,30 +1,47 @@
-import React from 'react'
-import '../css/Modal.css'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../css/Modal.css";
+import { Link } from "react-router-dom";
+import { FaTelegramPlane } from "react-icons/fa";
 
 const Modalbtcdeposit = () => {
   return (
-    <div className='modal-container'>
-        <div className='modal-box deposit' style={{overflowY: 'scroll'}}>
-            <h1>Deposit using bitcoin</h1>
-            <div>
-              <h3>Wallet Address</h3>
-              <p>qwertyui12345678</p>
-            </div>
-            <form style={{textAlign: 'left'}}>
-              <label>Deposit Plan</label>
-              <select>
-                <option>starter</option>
-                <option>siler</option>
-                <option>gold</option>
-              </select>
-            <h3>DEPOSIT PROOF</h3>
-            <input type='file'></input>
-            </form>
-            <Link to='/deposit'>back</Link>
+    <div className="modal-container">
+      <div className="modal-box deposit" style={{ overflowY: "scroll" }}>
+        <h2>Deposit using bitcoin</h2>
+        <div>
+          <h3>Wallet Address</h3>
+          <p>qwertyui12345678</p>
         </div>
+        <form>
+          <h3>Amount Deposited</h3>
+          <div>
+            <input type="number" placeholder="Amount Deposited" />
+          </div>
+          <br />
+          <h3>Deposit Proof</h3>
+          <div>
+            <input type="file" />
+          </div>
+          <br />
+          <button>
+            {" "}
+            <FaTelegramPlane /> Submit
+          </button>
+        </form>
+        <br />
+        <div>
+          <h3>How to deposit</h3>
+          <p>
+            You are required to make payment to the address provided above and
+            fill the form as stated for your payment to be processed.
+          </p>
+        </div>
+        <Link to="/deposit" style={{ marginTop: "0px" }}>
+          Go back
+        </Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modalbtcdeposit
+export default Modalbtcdeposit;
--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,20 +1,20 @@
-import React from 'react'
-import '../css/Modal.css'
-import { AiOutlineClose } from 'react-icons/ai'
-import { Link } from 'react-router-dom'
+import React from "react";
+import "../css/Modal.css";
+import { AiOutlineClose } from "react-icons/ai";
+import { Link } from "react-router-dom";
 
-const Modals = ({openModal}) => {
+const Modals = ({ openModal }) => {
   return (
-    <div className='modal-container'>
-        <div className='modal-box'>
-            <span>
-              <h1 style={{margin: '0px'}}>Hello, ready to invest?</h1>
-              <AiOutlineClose onClick={()=>openModal()} className='icon'/>
-            </span>
-            <Link to='/deposit'>continue to deposit</Link>
-        </div>
+    <div className="modal-container">
+      <div className="modal-box">
+        <span>
+          <h1 style={{ margin: "0px" }}>Hello, ready to invest?</h1>
+          <AiOutlineClose onClick={() => openModal()} className="icon" />
+        </span>
+        <Link to="/deposit">continue to deposit</Link>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modals
+export default Modals;
