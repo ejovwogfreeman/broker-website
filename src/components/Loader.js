fe5c37912f4ecb57c034d030da302389733f9f2a
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-10-01 14:19:18 +0100

    message

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,15 +1,19 @@
-import React from 'react'
+import React from "react";
 // import logo from "../assets/winnerslogo.png";
-import loader from '../assets/loading.gif'
-
+import loader from "../assets/loading.gif";
 
 const Loader = () => {
   return (
-    <div style={{textAlign: 'center', marginTop: '250px'}}>
-        {/* <img src={logo} alt="" width='100px'style={{display: 'block', margin: 'auto'}}/> */}
-        <img src={loader} alt="loading" width='100px'style={{display: 'block', margin: 'auto'}}/>
+    <div style={{ textAlign: "center", marginTop: "100px" }}>
+      {/* <img src={logo} alt="" width='100px'style={{display: 'block', margin: 'auto'}}/> */}
+      <img
+        src={loader}
+        alt="loading"
+        width="100px"
+        style={{ display: "block", margin: "auto" }}
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Loader;
