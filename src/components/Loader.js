8b9c4768cc43dd1c3da4e300b25e053a14c1084e
ejovwogfreeman <ejovwogfreeman007@gmail.com>
2022-10-01 14:20:58 +0100

    message

--- src/components/Loader.js
+++ src/components/Loader.js
@@ -1,11 +1,16 @@
 import React from "react";
-// import logo from "../assets/winnerslogo.png";
+import logo from "../assets/logo.png";
 import loader from "../assets/loading.gif";
 
 const Loader = () => {
   return (
     <div style={{ textAlign: "center", marginTop: "100px" }}>
-      {/* <img src={logo} alt="" width='100px'style={{display: 'block', margin: 'auto'}}/> */}
+      <img
+        src={logo}
+        alt=""
+        width="100px"
+        style={{ display: "block", margin: "auto" }}
+      />
       <img
         src={loader}
         alt="loading"
